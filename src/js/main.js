e15864f8d6f6a294596d92460200b58a1e13d087
Annelie Svensson <annelievs@hotmail.com>
2024-01-29 11:03:34 +0100

    Lagt till sökbar + styling samt funktion för sortering och filtrering

--- src/js/main.js
+++ src/js/main.js
@@ -1,70 +1,77 @@
-"use strict" ;
+"use strict";
 
-// Variabler
+//Variabler
 const toggleBtn = document.querySelector('.toggle-btn');
 const toggleBtnIcon = document.querySelector('.toggle-btn i');
 const dropDownMenu = document.querySelector('.dropdown-menu');
-
 const url = "https://dahlgren.miun.se/ramschema_ht23.php";
 const codeEl = document.getElementById("code");
 const nameEl = document.getElementById("name");
 const progressionEl = document.getElementById("progression");
+const queryEl = document.getElementById("query");
+let courses = [];
 
-//Händelsehanterare
-codeEl.addEventListener("click", sortByCode, false);
-nameEl.addEventListener("click", sortByName, false);
-progressionEl.addEventListener("click", sortByProgression, false);
+// Händelsehanterare
+toggleBtn.addEventListener("click", toggleMenu, false);
+codeEl.addEventListener("click", function () { sortCourses("code"); }, false);
+nameEl.addEventListener("click", function () { sortCourses("coursename"); }, false);
+progressionEl.addEventListener("click", function () { sortCourses("progression"); }, false);
+queryEl.addEventListener("input", searchCourses, false);
 
-window.onload = init;
+window.onload = getCourses();
 
 // Funktion för att öppna och stänga menyn i mobilläge
-toggleBtn.onclick = function() {
+function toggleMenu() {
     dropDownMenu.classList.toggle('open');
     const isOpen = dropDownMenu.classList.contains('open');
 
-    if (isOpen) {
-        toggleBtnIcon.classList = 'fa-solid fa-xmark';
-    } else {
-        toggleBtnIcon.classList = 'fa-solid fa-bars';
-    }
-};
+    toggleBtnIcon.className = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+}
 
-
-async function init() {
+// Funktion för att hämta information om kurser
+async function getCourses() {
     try {
-        //Fetch-anrop
+        // Fetch-anrop
         const response = await fetch(url);
-        const courses = await response.json();
+        courses = await response.json();
 
-    displayCourses(courses);
-    
-    } catch {
+        // Anropa funktion för att skriva ut kurserna till DOM
+        displayCourses(courses);
+
+    } catch (error) {
         document.getElementById("error").innerHTML = "<p>Något gick fel, prova igen senare!</p>";
     }
-};
+}
 
+// Funktion som skriver ut information om kurserna till DOM
 function displayCourses(courses) {
     const coursesEl = document.getElementById("courses-list");
+    coursesEl.replaceChildren();
 
-courses.forEach((course) => {
-   coursesEl.innerHTML += `
-   <tr>
-   <td>${course.code}</td>
-   <td>${course.coursename}</td>
-   <td>${course.progression}</td>
-   </tr>
-   `;
-});
+    //Loopa igenom arrayen och skriv ut till DOM
+    courses.forEach((course) => {
+        coursesEl.innerHTML += `
+        <tr>
+        <td>${course.code}</td>
+        <td>${course.coursename}</td>
+        <td>${course.progression}</td>
+        </tr>
+        `;
+    });
 }
 
-function sortByCode() {
-    courses.sort((a, b) => (a.code > b.code) ? 1 : -1);
+// Funktion för att sortera kurserna
+function sortCourses(sortType) {
+    courses.sort((a, b) => (a[sortType] > b[sortType]) ? 1 : -1);
+    displayCourses(courses);
 }
 
-function sortByName() {
-    courses.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1);
-}
+// Filtrera kurser baserat på kurskod eller kursnamn
+function searchCourses() {
+    const query = queryEl.value.toLowerCase();
+    const filteredCourses = courses.filter((course) => {
+        return course.code.toLowerCase().includes(query) || course.coursename.toLowerCase().includes(query);
+    });
 
-function sortByProgression() {
-    courses.sort((a, b) => (a.progression > b.progression) ? 1 : -1);
+    displayCourses(filteredCourses);
 }
